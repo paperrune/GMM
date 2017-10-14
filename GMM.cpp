d092480991cc6d2e33b03c2335e80589c28cbada
paperrune <paperrune@naver.com>
2017-10-14 19:50:00 +0900

    Update GMM.cpp

--- GMM.cpp
+++ GMM.cpp
@@ -12,28 +12,52 @@ Gaussian_Mixture_Model::Gaussian_Mixture_Model(char type_covariance[], int dimen
 	this->dimension_data			 = dimension_data;
 	this->number_gaussian_components = number_gaussian_components;
 
-	covariance	= new double**[number_gaussian_components];
-	mean		= new double*[number_gaussian_components];
-	weight		= new double[number_gaussian_components];
+	mean	= new double*[number_gaussian_components];
+	weight	= new double[number_gaussian_components];
 			
 	for(int i = 0;i < number_gaussian_components;i++){
-		covariance[i]	= new double*[dimension_data];
-		mean[i]			= new double[dimension_data];
-			
-		for(int j = 0;j < dimension_data;j++){
-			covariance[i][j] = new double[dimension_data];
+		mean[i] = new double[dimension_data];
+	}
+
+	if(!strcmp(type_covariance, "diagonal")){
+		diagonal_covariance = new double*[number_gaussian_components];
+
+		for(int i = 0;i < number_gaussian_components;i++){
+			diagonal_covariance[i] = new double[dimension_data];
 		}
 	}
+	else{
+		covariance = new double**[number_gaussian_components];
+
+		for(int i = 0;i < number_gaussian_components;i++){
+			covariance[i] = new double*[dimension_data];
+			
+			for(int j = 0;j < dimension_data;j++){
+				covariance[i][j] = new double[dimension_data];
+			}
+		}
+	}	
 }
 Gaussian_Mixture_Model::~Gaussian_Mixture_Model(){
-	for(int i = 0;i < number_gaussian_components;i++){
-		for(int j = 0;j < dimension_data;j++){
-			delete[] covariance[i][j];
+	if(!strcmp(type_covariance, "diagonal")){
+		for(int i = 0;i < number_gaussian_components;i++){
+			delete[] diagonal_covariance[i];
 		}
-		delete[] mean[i];
-		delete[] covariance[i];
+		delete[] diagonal_covariance;
 	}
-	delete[] covariance;
+	else{
+		for(int i = 0;i < number_gaussian_components;i++){
+			for(int j = 0;j < dimension_data;j++){
+				delete[] covariance[i][j];
+			}
+			delete[] covariance[i];
+		}
+		delete[] covariance;
+	}
+
+	for(int i = 0;i < number_gaussian_components;i++){
+		delete[] mean[i];
+	}
 	delete[] mean;
 	delete[] weight;
 }
@@ -45,9 +69,14 @@ void Gaussian_Mixture_Model::Initialize(int number_data, double **data){
 	while(kmeans.Cluster(number_data, data));
 
 	for(int i = 0;i < number_gaussian_components;i++){
-		for(int j = 0;j < dimension_data;j++){	
-			for(int k = 0;k < dimension_data;k++){
-				covariance[i][j][k] = (j == k);
+		for(int j = 0;j < dimension_data;j++){
+			if(!strcmp(type_covariance, "diagonal")){
+				diagonal_covariance[i][j] = 1;
+			}
+			else{
+				for(int k = 0;k < dimension_data;k++){
+					covariance[i][j][k] = (j == k);
+				}
 			}
 		}
 		for(int j = 0;j < dimension_data;j++){				
@@ -70,10 +99,19 @@ void Gaussian_Mixture_Model::Load_Parameter(char path[]){
 			}
 		}
 
-		for(int i = 0;i < number_gaussian_components;i++){
-			for(int j = 0;j < dimension_data;j++){
-				for(int k = 0;k < dimension_data;k++){
-					fscanf(file, "%lf", &covariance[i][j][k]);
+		if(!strcmp(type_covariance, "diagonal")){
+			for(int i = 0;i < number_gaussian_components;i++){
+				for(int j = 0;j < dimension_data;j++){
+					fscanf(file, "%lf", &diagonal_covariance[i][j]);
+				}
+			}
+		}
+		else{
+			for(int i = 0;i < number_gaussian_components;i++){
+				for(int j = 0;j < dimension_data;j++){
+					for(int k = 0;k < dimension_data;k++){
+						fscanf(file, "%lf", &covariance[i][j][k]);
+					}
 				}
 			}
 		}
@@ -96,10 +134,19 @@ void Gaussian_Mixture_Model::Save_Parameter(char path[]){
 		}
 	}
 
-	for(int i = 0;i < number_gaussian_components;i++){
-		for(int j = 0;j < dimension_data;j++){
-			for(int k = 0;k < dimension_data;k++){
-				fprintf(file, "%f\n", covariance[i][j][k]);
+	if(!strcmp(type_covariance, "diagonal")){
+		for(int i = 0;i < number_gaussian_components;i++){
+			for(int j = 0;j < dimension_data;j++){
+				fprintf(file, "%f\n", diagonal_covariance[i][j]);
+			}
+		}
+	}
+	else{
+		for(int i = 0;i < number_gaussian_components;i++){
+			for(int j = 0;j < dimension_data;j++){
+				for(int k = 0;k < dimension_data;k++){
+					fprintf(file, "%f\n", covariance[i][j][k]);
+				}
 			}
 		}
 	}
@@ -145,40 +192,68 @@ double Gaussian_Mixture_Model::Expectaion_Maximization(int number_data, double *
 	double *sum_likelihood			= new double[number_gaussian_components];
 		
 	double **new_mean = new double*[number_gaussian_components];
-		
-	double ***new_covariance = new double**[number_gaussian_components];
-		
-	for(int i = 0;i < number_gaussian_components;i++){
-		new_mean[i]			= new double[dimension_data];
-		new_covariance[i]	= new double*[dimension_data];
-			
-		for(int j = 0;j < dimension_data;j++){
-			new_covariance[i][j] = new double[dimension_data];
-		}
-	}
 
+	double **new_diagonal_covariance;
+	double ***new_covariance;
+		
 	for(int i = 0;i < number_gaussian_components;i++){
+		new_mean[i] = new double[dimension_data];
+
 		for(int j = 0;j < dimension_data;j++){
-			for(int k = 0;k < dimension_data;k++){
-				new_covariance[i][j][k] = 0;
-			}
 			new_mean[i][j] = 0;
 		}
 		sum_likelihood[i] = 0;
+	}
+
+	if(!strcmp(type_covariance, "diagonal")){
+		new_diagonal_covariance = new double*[number_gaussian_components];
+
+		for(int i = 0;i < number_gaussian_components;i++){
+			new_diagonal_covariance[i] = new double[dimension_data];
+
+			for(int j = 0;j < dimension_data;j++){
+				new_diagonal_covariance[i][j] = 0;
+			}
+		}
+	}
+	else{
+		new_covariance = new double**[number_gaussian_components];
+
+		for(int i = 0;i < number_gaussian_components;i++){
+			new_covariance[i] = new double*[dimension_data];
+
+			for(int j = 0;j < dimension_data;j++){
+				new_covariance[i][j] = new double[dimension_data];
+
+				for(int k = 0;k < dimension_data;k++){
+					new_covariance[i][j][k] = 0;
+				}
+			}
+		}
 	}
 
 	for(int i = 0;i < number_data;i++){
 		double sum = 0;
 				
 		for(int j = 0;j < number_gaussian_components;j++){
-			sum += weight[j] * (gaussian_distribution[j] = Gaussian_Distribution(data[i], mean[j], covariance[j]));
+			if(!strcmp(type_covariance, "diagonal")){
+				sum += weight[j] * (gaussian_distribution[j] = Gaussian_Distribution(data[i], mean[j], diagonal_covariance[j]));
+			}
+			else{
+				sum += weight[j] * (gaussian_distribution[j] = Gaussian_Distribution(data[i], mean[j], covariance[j]));
+			}
 		}
 		for(int j = 0;j < number_gaussian_components;j++){
 			double likelihood = weight[j] * gaussian_distribution[j] / sum;
 
 			for(int k = 0;k < dimension_data;k++){
-				for(int l = 0;l < dimension_data;l++){
-					new_covariance[j][k][l] += likelihood * (data[i][k] - mean[j][k]) * (data[i][l] - mean[j][l]);
+				if(!strcmp(type_covariance, "diagonal")){
+					new_diagonal_covariance[j][k] += likelihood * (data[i][k] - mean[j][k]) * (data[i][k] - mean[j][k]);
+				}
+				else{
+					for(int l = 0;l < dimension_data;l++){
+						new_covariance[j][k][l] += likelihood * (data[i][k] - mean[j][k]) * (data[i][l] - mean[j][l]);
+					}
 				}
 				new_mean[j][k] += likelihood * data[i][k];
 			}
@@ -188,8 +263,13 @@ double Gaussian_Mixture_Model::Expectaion_Maximization(int number_data, double *
 
 	for(int i = 0;i < number_gaussian_components;i++){						
 		for(int j = 0;j < dimension_data;j++){
-			for(int k = 0;k < dimension_data;k++){
-				covariance[i][j][k] = new_covariance[i][j][k] / sum_likelihood[i];
+			if(!strcmp(type_covariance, "diagonal")){
+				diagonal_covariance[i][j] = new_diagonal_covariance[i][j] / sum_likelihood[i];
+			}
+			else{
+				for(int k = 0;k < dimension_data;k++){
+					covariance[i][j][k] = new_covariance[i][j][k] / sum_likelihood[i];
+				}
 			}
 			mean[i][j] = new_mean[i][j] / sum_likelihood[i];
 		}
@@ -199,23 +279,57 @@ double Gaussian_Mixture_Model::Expectaion_Maximization(int number_data, double *
 	for(int i = 0;i < number_data;i++){
 		log_likelihood += log(Calculate_Likelihood(data[i]));
 	}
-		
-	for(int i = 0;i < number_gaussian_components;i++){
-		for(int j = 0;j < dimension_data;j++){
-			delete[] new_covariance[i][j];
+
+	if(!strcmp(type_covariance, "diagonal")){
+		for(int i = 0;i < number_gaussian_components;i++){
+			delete[] new_diagonal_covariance[i];
 		}
+		delete[] new_diagonal_covariance;
+	}
+	else{
+		for(int i = 0;i < number_gaussian_components;i++){
+			for(int j = 0;j < dimension_data;j++){
+				delete[] new_covariance[i][j];
+			}
+			delete[] new_covariance[i];
+		}
+		delete[] new_covariance;
+	}
+
+	for(int i = 0;i < number_gaussian_components;i++){
 		delete[] new_mean[i];
-		delete[] new_covariance[i];
 	}
 	delete[] gaussian_distribution;
-	delete[] new_covariance;
 	delete[] new_mean;
 	delete[] sum_likelihood;
 
 	return log_likelihood;
 }
 double Gaussian_Mixture_Model::Gaussian_Distribution(double data[], int component_index){
-	return Gaussian_Distribution(data, mean[component_index], covariance[component_index]);
+	int j = component_index;
+
+	if(!strcmp(type_covariance, "diagonal")){
+		return Gaussian_Distribution(data, mean[j], diagonal_covariance[j]);
+	}
+	else{
+		return Gaussian_Distribution(data, mean[j], covariance[j]);
+	}
+}
+double Gaussian_Mixture_Model::Gaussian_Distribution(double data[], double mean[], double diagonal_covariance[]){
+	double determinant = 1;
+	double result;
+	double sum = 0;
+		
+	for(int i = 0;i < dimension_data;i++){
+		determinant *= diagonal_covariance[i];
+		sum			+= (data[i] - mean[i]) * (1 / diagonal_covariance[i]) * (data[i] - mean[i]);
+	}
+	result = 1.0 / (pow(2 * 3.1415926535897931, dimension_data / 2.0) * sqrt(determinant)) * exp(-0.5 * sum);
+
+	if(_isnan(result) || !_finite(result)){
+		fprintf(stderr, "[Gaussian Distribution], [The covariance matrix is rank deficient], [result: %lf]\n", result);
+	}
+	return result;
 }
 double Gaussian_Mixture_Model::Gaussian_Distribution(double data[], double mean[], double **covariance){
 	double result;
