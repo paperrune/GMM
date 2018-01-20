42974ed2d4585244682efddf8495a6c735a19baf
paperrune <paperrune@naver.com>
2018-01-20 19:46:42 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <fstream>
 
 #include "GMM.h"
 
 int main(){
-	char type_covariance[] = "full"; // <-> "diagonal"
-	
+	string type_covariance = "full"; // <-> "diagonal"
+
 	int dimension_data		= 2;
-	int number_data			= 300;
-	int number_iterations	= 100;
+	int number_data			= 100;
+	int number_iterations	= 999;
 
 	int number_gaussian_components = 4;
 
 	double **data = new double*[number_data];
 
-	FILE *file;
+	ofstream file;
 
 	Gaussian_Mixture_Model GMM = Gaussian_Mixture_Model(type_covariance, dimension_data, number_gaussian_components);
 
-	for(int i = 0;i < number_data;i++){
-		double position[] = {0.25, 0.75};
+	for (int i = 0; i < number_data; i++){
+		double position[] = { 0.25, 0.75 };
 
-		data[i]		= new double[dimension_data];
-		data[i][0]	= 0.25 * rand() / RAND_MAX - 0.125 + position[(i % 2)];
-		data[i][1]	= 0.25 * rand() / RAND_MAX - 0.125 + position[(i % 4) / 2];
+		data[i] = new double[dimension_data];
+		data[i][0] = 0.25 * rand() / RAND_MAX - 0.125 + position[(i % 2)];
+		data[i][1] = 0.25 * rand() / RAND_MAX - 0.125 + position[(i % 4) / 2];
 	}
 
 	printf("step	log_likelihood\n");
-	for(int i = 0;i < number_iterations;i++){
+	for (int i = 0; i < number_iterations; i++){
 		double log_likelihood;
 
-		if(i == 0) GMM.Initialize(number_data, data);
+		if (i == 0) GMM.Initialize(number_data, data);
 
 		log_likelihood = GMM.Expectaion_Maximization(number_data, data);
-		if((i + 1) % 10 == 0) printf("%d	%lf\n", i + 1, log_likelihood);
+		if ((i + 1) % 10 == 0) printf("%d	%lf\n", i + 1, log_likelihood);
 	}
 
 	printf("\nmean\n");
-	for(int i = 0;i < number_gaussian_components;i++){
-		for(int j = 0;j < dimension_data;j++){
+	for (int i = 0; i < number_gaussian_components; i++){
+		for (int j = 0; j < dimension_data; j++){
 			printf("%lf ", GMM.mean[i][j]);
 		}
 		printf("\n");
 	}
 
-	file = fopen("result.txt", "wt");
+	file.open("result.txt");
 
-	for(int j = 0;j < number_gaussian_components;j++){
-		for(int i = 0;i < number_data;i++){
-			if(GMM.Classify(data[i]) == j){
-				fprintf(file, "%d %lf %lf\n", GMM.Classify(data[i]), data[i][0], data[i][1]);
+	for (int j = 0; j < number_gaussian_components; j++){
+		for (int i = 0; i < number_data; i++){
+			if (GMM.Classify(data[i]) == j){
+				file << GMM.Classify(data[i]) << " " << data[i][0] << " " << data[i][1] << endl;
 			}
 		}
 	}
-	fclose(file);
+	file.close();
 
-	for(int i = 0;i < number_data;i++){
+	for (int i = 0; i < number_data; i++){
 		delete[] data[i];
 	}
 	delete[] data;
